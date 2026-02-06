ce1694b867ac1a4994e1d2ce1da20c6043f93de2
Nauman Shiraz <nauman_shiraz@hotmail.com>
2026-02-06 18:25:46 +0500

    commit to repo

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { HashRouter } from "react-router-dom";
+
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import '../index.css';
 import App from '@/app/App';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
